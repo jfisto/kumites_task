d64bdb798e5ec7f724aedf06af3ba2b2bedc9695
jfisto <ale1431999@gmail.com>
2023-06-17 01:02:32 +0300

    Update find_array

--- src/kumites/kumite_array.py
+++ src/kumites/kumite_array.py
@@ -1,6 +1,6 @@
 class KumiteArray:
     def find_array(self, arr_first, arr_second):
-        return [arr_first[i] for i in arr_second if i <= len(arr_first)]
+        return [arr_first[i] for i in arr_second if i < len(arr_first)]
 
 
 if __name__ == '__main__':
