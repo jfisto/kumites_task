7e30de040a3e6dbc75eb7b543d8ae5d1e65d7ca2
jfisto <ale1431999@gmail.com>
2023-06-17 03:26:58 +0300

    Update find_array

--- src/kumites/kumite_array.py
+++ src/kumites/kumite_array.py
@@ -2,7 +2,12 @@ class KumiteArray:
     def find_array(self, arr_first, arr_second):
         return [arr_first[i] for i in arr_second if i < len(arr_first)]
 
+    def solve(self,word):
+        import re
+        return 1#max([sum([ord(letter) - ord('a') + 1 for letter in consOnly]) for consOnly in re.split("a|e|i|o|u", word)])
+
 
 if __name__ == '__main__':
     obj = KumiteArray()
-    print(obj.find_array([0, 3, 4], [2, 6]))
+    print(obj.solve(1))
+    # print(obj.find_array([0, 3, 4], [2, 6]))
